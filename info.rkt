617789f7c610973ab316b6189093510666d0c0e9
Leif Andersen <leif@leifandersen.net>
2015-02-01 09:28:38 -0500

    Fix dependencies

--- info.rkt
+++ info.rkt
@@ -1,7 +1,5 @@
 #lang info
 (define collection 'multi)
 (define version    "1")
-(define deps '("mzscheme"))
+(define deps '("mzscheme" "base" "compatibility-lib" "gui-lib" "scheme-lib"))
 (define build-deps '("at-exp-lib" "mzscheme" "scribble-lib" "racket-doc"))
-
-
