533f77531e5352a10cd792f88d985bc5ee39b9dc
Leif Andersen <leif@leifandersen.net>
2016-03-18 13:48:26 -0400

    add rnrs-lib to the deps.

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang info
 (define collection 'multi)
 (define version    "1")
-(define deps '("mzscheme" "base" "compatibility-lib" "gui-lib" "scheme-lib"))
+(define deps '("mzscheme" "base" "compatibility-lib" "gui-lib" "scheme-lib" "r5rs-lib"))
 (define build-deps '("at-exp-lib" "mzscheme" "scribble-lib" "racket-doc"))
