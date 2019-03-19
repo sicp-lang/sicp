200c6c131a8e4572d21438c8a5f12fc684f8deb2
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2019-03-18 23:46:23 -0700

    Fix dependencies

--- info.rkt
+++ info.rkt
@@ -1,5 +1,17 @@
 #lang info
+
 (define collection 'multi)
 (define version    "1")
-(define deps '("mzscheme" "base" "compatibility-lib" "gui-lib" "scheme-lib" "r5rs-lib"))
-(define build-deps '("at-exp-lib" "mzscheme" "scribble-lib" "racket-doc"))
+(define deps '("draw-lib"
+               "snip-lib"
+               "mzscheme"
+               "base"
+               "compatibility-lib"
+               "gui-lib"
+               "scheme-lib"
+               "r5rs-lib"))
+(define build-deps '("r5rs-doc"
+                     "at-exp-lib"
+                     "mzscheme"
+                     "scribble-lib"
+                     "racket-doc"))
