f0b127981e04d85cab8e1810ff12a045f65c6a38
Jens Axel Søgaard <jensaxel@soegaard.net>
2022-06-27 10:37:56 +0200

    Revert "Add type version for `sicp-pict` and make some improvements to `sicp` and `sicp-pict`."

--- sicp/main.rkt
+++ sicp/main.rkt
@@ -2,8 +2,7 @@
 
 (require racket/provide
          (prefix-in r5rs: r5rs)
-         (only-in r5rs [lambda r5rs:λ])
-         (only-in racket/base [random racket:random]))
+         (rename-in racket [random racket:random]))
 
 (provide (filtered-out (λ (name) (regexp-replace #px"^r5rs:" name ""))
                        (except-out (all-from-out r5rs) r5rs:#%module-begin))
@@ -11,26 +10,22 @@
 
 (define-syntax (define+provide stx)
   (syntax-case stx ()
-    [(_ (id . args) . body) #'(define+provide id (λ args . body))]
+    [(_ (id . args) . body) #'(begin
+                                (provide id)
+                                (define (id . args) . body))]
     [(_ id expr) #'(begin
                      (provide id)
-                     (define id
-                       (letrec ([id expr])
-                         (if (and (procedure? id)
-                                  (not (eq? (object-name id) 'id)))
-                             (procedure-rename id 'id)
-                             id))))]))
+                     (define id expr))]))
 
-(provide true false
-         raise error
-         compose compose1 identity
-         empty? empty null)
-(define+provide inc add1)
-(define+provide dec sub1)
-(define+provide 1+  add1)
-(define+provide 1-  sub1)
-(define+provide -1+ sub1)
+(provide true)
+(provide false)
+(provide error)
+(provide identity)
 (define+provide nil '())
+(define+provide the-empty-stream '())
+(define+provide stream-null? null?)
+(define+provide (inc x) (+ x 1))
+(define+provide (dec x) (- x 1))
 (define+provide (runtime)
   (inexact->exact (truncate (* 1000 (current-inexact-milliseconds)))))
 (define+provide (random n)
@@ -38,31 +33,10 @@
       (racket:random n)
       (* n (racket:random))))
 
-(provide cons-stream stream* stream
-         (rename-out
-          [the-empty-stream empty-stream]
-          [cons-stream      stream-cons]
-          [stream-null?     stream-empty?]
-          [stream-car       stream-first]
-          [stream-cdr       stream-rest]))
+(provide cons-stream)
 (define-syntax cons-stream
   (syntax-rules ()
     [(_ A B) (r5rs:cons A (r5rs:delay B))]))
-(define-syntax stream*
-  (syntax-rules ()
-    [(_ A) A]
-    [(_ A B ...) (cons-stream A (stream* B ...))]))
-(define-syntax stream
-  (syntax-rules ()
-    [(_ A ...) (stream* A ... the-empty-stream)]))
-(define+provide the-empty-stream '())
-(define+provide stream-null? null?)
-(define+provide (stream? v)
-  (or (stream-null? v)
-      (and (r5rs:pair? v)
-           #;(r5rs:promise? (r5rs:cdr v)))))
-(define+provide stream-car r5rs:car)
-(define+provide stream-cdr (compose1 r5rs:force r5rs:cdr))
 
 
 (provide amb)
@@ -71,23 +45,20 @@
 (define (set-amb-fail! x) (set! amb-fail x))
 
 (define-syntax-rule (explore +prev-amb-fail +sk alt)
-  (let/cc +fk
-    (set-amb-fail!
-     (thunk
-      (set-amb-fail! +prev-amb-fail)
-      (+fk 'fail)))
-    (+sk alt)))
+  (call/cc
+   (lambda (+fk)
+     (set-amb-fail!
+      (thunk
+       (set-amb-fail! +prev-amb-fail)
+       (+fk 'fail)))
+     (+sk alt))))
 
-(define-syntax amb
-  (syntax-rules ()
-    [(_) (amb-fail)]
-    [(_ alt) alt]
-    [(_ alt ...)
-     (let ([+prev-amb-fail amb-fail])
-       (let/cc +sk
-         (explore +prev-amb-fail +sk alt)
-         ...
-         (+prev-amb-fail)))]))
+(define-syntax-rule (amb alt ...)
+  (let ([+prev-amb-fail amb-fail])
+    (call/cc
+     (lambda (+sk)
+       (explore +prev-amb-fail +sk alt) ...
+       (+prev-amb-fail)))))
 
 (define-syntax module-begin
   (syntax-rules ()
